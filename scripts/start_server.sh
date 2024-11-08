8c49d20cf13d12f22d171a6b039e4ad7f8c3b984
Angela Yu <ayu107@my.bcit.ca>
2024-11-07 22:40:48 -0800

    add sudo

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 # Install Apache if it’s not already installed
 yum install -y httpd
 # Start Apache if it’s not running
-systemctl start httpd
-systemctl enable httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
